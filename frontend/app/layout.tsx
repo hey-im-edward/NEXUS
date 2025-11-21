de8e99cbcdd7ec7ac66d9caac1d6cb74033c9dd8
hey-im-edward <hey_im_edward@icloud.com>
2025-11-21 20:24:21 +0700

    temp: save frontend changes before reset to 17/11

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,3 @@
-import { QueryProvider } from "@/components/providers/query-provider";
 import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -29,10 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QueryProvider>
-          {children}
-          <Toaster />
-        </QueryProvider>
+        {children}
+        <Toaster />
       </body>
     </html>
   );
